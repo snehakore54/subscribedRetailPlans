f36e5bdef874add6dca5af55079cc1883b59f6e6
snehakore54 <151037050+snehakore54@users.noreply.github.com>
2024-04-25 20:56:25 +0530

    first proj

---
+++ src/App.test.js
@@ -0,0 +1,20 @@
+/*import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+*/
+import { render, screen, waitFor } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', async () => {
+  render(<App />);
+  // Wait for the text to appear
+  await waitFor(() => {
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
